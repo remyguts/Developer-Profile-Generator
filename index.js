8704253c44ab4af3bf8db1cdcebb2b0a87a1ae11
Remy Guts <remyguts@gmail.com>
2020-02-02 21:05:28 -0800

    finishing the last touches until I freak out and change a buch of stuff again

--- index.js
+++ index.js
@@ -36,30 +36,55 @@ function promptUser() {
   ]);
 }
 
-function generateHTML() {
-  return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is Remy</h1>
-    <p class="lead">I am from Seattle.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is remyguts</li>
-      <li class="list-group-item">LinkedIn: blahhhh</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+function generateHTML(
+  name,
+  avatar_url,
+  location,
+  blog,
+  bio,
+  followers,
+  following,
+  html_url,
+  starred_url,
+  public_repos
+) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  
+    <title>Document</title>
+  </head>
+  
+  <body id="yo">
+    <div class="jumbotron jumbotron-fluid" id="this">
+      <div class="container" id="that">
+        <h1 class="display-4"> ${name}</h1>
+        <img src='${avatar_url}' id="mage">
+        <p class="lead">I am from ${location}.</p>
+        <p> ${bio} </p>
+        <ul class="list-group">
+          <li class="list-group-item" id="homie"><a href="${html_url}"> </a>My GitHub </li>
+           <li class="list-group-item" id="homie"> LinkedIn:><a
+                href="${blog}">Linkedin</a>
+          </li>
+          <li class="list-group-item" id="homie"> Number of Followers:  ${followers} </li>
+          <li class="list-group-item" id="homie"> Number of Repos: ${public_repos} </li>
+          <li class="list-group-item" id="homie"> Pages I Follow:${following}</li>
+          <li class="list-group-item" id="homie"><a
+              href="${starred_url}"></a>Starred
+            Repos </li>
+        </ul>
+      </div>
+    </div>
+  </body>
+  
+  </html>
+  `;
 }
 
 async function profGEn() {
@@ -67,8 +92,23 @@ async function profGEn() {
     const { name, favcolor } = await promptUser();
     const queryUrl = `https://api.github.com/users/${name}`;
     const { data } = await axios.get(queryUrl);
-    console.log(name, favcolor, data);
-    await writeFileAsync("index.html", generateHTML(), "utf8");
+    console.log(name, favcolor, data.avatar_url);
+    await writeFileAsync(
+      "index.html",
+      generateHTML(
+        name,
+        data.avatar_url,
+        data.location,
+        data.blog,
+        data.bio,
+        data.followers,
+        data.following,
+        data.html_url,
+        data.starred_url,
+        data.public_repos
+      ),
+      "utf8"
+    );
     printPDF();
   } catch (error) {
     console.log(error);
